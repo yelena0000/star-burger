1f660935061b01113291af1c04c33b720c9147fa
yelena0000 <kirillnek07081999@gmail.com>
2025-05-10 21:01:01 +0300

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -99,9 +99,12 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+        default=env.str('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
