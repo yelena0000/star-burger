02a7584228486aacdf334a3915e230851abe3c95
yelena0000 <kirillnek07081999@gmail.com>
2025-07-14 01:06:54 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ docker-compose -f docker-compose.prod.yaml exec backend python manage.py collect
 docker cp $(docker-compose -f docker-compose.prod.yaml ps -q backend):/app/staticfiles /opt/star-burger/staticfiles
 
 sudo cp -r /opt/star-burger/bundles/* /var/www/frontend/
-sudo cp -r /opt/star-burger/staticfiles/* /var/www/frontend/
+sudo cp -r /opt/star-burger/staticfiles/staticfiles/* /var/www/frontend/
 
 sudo systemctl restart nginx
