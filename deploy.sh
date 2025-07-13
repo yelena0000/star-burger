f99f920b7cebcf80dfbc6a89d364f03baab4752a
yelena0000 <kirillnek07081999@gmail.com>
2025-07-13 19:46:10 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git pull
 docker-compose -f docker-compose.prod.yaml down
 docker-compose -f docker-compose.prod.yaml up --build -d
 docker-compose -f docker-compose.prod.yaml run backend python manage.py migrate
-docker-compose -f docker-compose.prod.yaml run backend python manage.py collectstatic --noinput
-sudo cp -r bundles/* /var/www/frontend/
-sudo cp -r staticfiles/* /var/www/frontend/
+docker-compose -f docker-compose.prod.yaml exec backend python manage.py collectstatic --noinput
+sudo cp -r /opt/star-burger/bundles/* /var/www/frontend/
+sudo cp -r /opt/star-burger/staticfiles/* /var/www/frontend/
 sudo systemctl restart nginx
