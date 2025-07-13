46b7b29ca83169bf4e7f8d876daf7e29067d7f56
yelena0000 <kirillnek07081999@gmail.com>
2025-07-13 15:55:44 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git pull
+docker-compose -f docker-compose.prod.yaml down
+docker-compose -f docker-compose.prod.yaml up --build -d
+docker-compose -f docker-compose.prod.yaml run backend python manage.py migrate
+docker-compose -f docker-compose.prod.yaml run backend python manage.py collectstatic --noinput
+sudo cp -r dist/* /var/www/frontend/
+sudo cp -r staticfiles/* /var/www/frontend/
+sudo systemctl restart nginx
