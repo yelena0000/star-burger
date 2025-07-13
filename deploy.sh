39dd69d06a738d285069506a8c6a3f27a2dcc007
yelena0000 <kirillnek07081999@gmail.com>
2025-07-13 20:02:59 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,15 @@ cd /opt/star-burger
 git pull
 docker-compose -f docker-compose.prod.yaml down
 docker-compose -f docker-compose.prod.yaml up --build -d
+
+docker cp $(docker-compose -f docker-compose.prod.yaml ps -q frontend):/app/bundles /opt/star-burger/bundles
+
 docker-compose -f docker-compose.prod.yaml run backend python manage.py migrate
 docker-compose -f docker-compose.prod.yaml exec backend python manage.py collectstatic --noinput
+
+docker cp $(docker-compose -f docker-compose.prod.yaml ps -q backend):/app/staticfiles /opt/star-burger/staticfiles
+
 sudo cp -r /opt/star-burger/bundles/* /var/www/frontend/
 sudo cp -r /opt/star-burger/staticfiles/* /var/www/frontend/
+
 sudo systemctl restart nginx
