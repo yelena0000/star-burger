01a6130ee675c31f14159661c2758fa16dd0db23
yelena0000 <kirillnek07081999@gmail.com>
2025-07-14 15:14:25 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,9 @@ sudo rm -rf /var/www/frontend/*
 docker-compose -f docker-compose.prod.yaml down
 docker-compose -f docker-compose.prod.yaml up --build -d
 
-#echo "[deploy] Копирую бандлы из контейнера..."
-#docker cp $(docker-compose -f docker-compose.prod.yaml ps -q frontend):/app/bundles /opt/star-burger/bundles
-
 echo "[deploy] Применяю миграции и собираю статику..."
-docker-compose -f docker-compose.prod.yaml run backend python manage.py migrate
+docker-compose -f docker-compose.prod.yaml exec backend python manage.py migrate
 docker-compose -f docker-compose.prod.yaml exec backend python manage.py collectstatic --noinput
-
-echo "[deploy] Копирую статику Django в /var/www/frontend..."
-docker cp $(docker-compose -f docker-compose.prod.yaml ps -q backend):/app/staticfiles/. /var/www/frontend/
 
 echo "[deploy] Копирую файлы бандлов в /var/www/frontend..."
 sudo cp -r /opt/star-burger/bundles/* /var/www/frontend/
